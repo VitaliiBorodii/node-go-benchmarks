91ad178bad7057dec87ab751afd15d7157e889fe
vitaliiborodii <allterraintacticalenforcer@gmail.com>
2017-06-14 09:38:17 +0300

    js code formatting

--- bench/binary-trees.js
+++ bench/binary-trees.js
@@ -3,52 +3,52 @@
  contributed by Isaac Gouy
  *reset*
  */
-const util = require('util');
+const util = require('util')
 
 class TreeNode {
   constructor(left, right) {
-    this.left = left;
-    this.right = right;
+    this.left = left
+    this.right = right
   }
 
   itemCheck() {
-    if (this.left == null) return 1;
-    return 1 + this.left.itemCheck() + this.right.itemCheck();
+    if (this.left == null) return 1
+    return 1 + this.left.itemCheck() + this.right.itemCheck()
   }
 }
 
 const bottomUpTree = (depth) => {
-  const arg = depth > 0 ? bottomUpTree(depth - 1) : null;
-  return new TreeNode(arg, arg);
-};
+  const arg = depth > 0 ? bottomUpTree(depth - 1) : null
+  return new TreeNode(arg, arg)
+}
 
 module.exports = (n = 0) => {
-  const start = Date.now();
-  const response = [];
+  const start = Date.now()
+  const response = []
 
 
-  const minDepth = 4;
-  const maxDepth = Math.max(minDepth + 2, n);
-  const stretchDepth = maxDepth + 1;
+  const minDepth = 4
+  const maxDepth = Math.max(minDepth + 2, n)
+  const stretchDepth = maxDepth + 1
 
-  let check = bottomUpTree(stretchDepth).itemCheck();
+  let check = bottomUpTree(stretchDepth).itemCheck()
 
-  response.push(util.format("stretch tree of depth %d\t check: %d\n", stretchDepth, check ));
+  response.push(util.format("stretch tree of depth %d\t check: %d\n", stretchDepth, check ))
 
-  const longLivedTree = bottomUpTree(maxDepth);
+  const longLivedTree = bottomUpTree(maxDepth)
 
   for (let depth = minDepth; depth <= maxDepth; depth += 2) {
-    const iterations = 1 << (maxDepth - depth + minDepth);
+    const iterations = 1 << (maxDepth - depth + minDepth)
 
-    check = 0;
+    check = 0
     for (let i = 1; i <= iterations; i++) {
-      check += bottomUpTree(depth).itemCheck();
+      check += bottomUpTree(depth).itemCheck()
     }
-    response.push(util.format("%d\t trees of depth %d\t check: %d\n", iterations, depth, check));
+    response.push(util.format("%d\t trees of depth %d\t check: %d\n", iterations, depth, check))
   }
 
-  response.push(util.format("long lived tree of depth %d\t check: %d\n", maxDepth, longLivedTree.itemCheck()));
-  response.push(util.format("Execution took: %d ms\n", Date.now() - start));
+  response.push(util.format("long lived tree of depth %d\t check: %d\n", maxDepth, longLivedTree.itemCheck()))
+  response.push(util.format("Execution took: %d ms\n", Date.now() - start))
 
-  return response;
-};
+  return response
+}
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const http = require('http');
-const util = require('util');
-const cluster = require('cluster');
-const path = require('path');
-const fs = require('fs');
-const os = require('os');
-const express = require('express');
+const http = require('http')
+const util = require('util')
+const cluster = require('cluster')
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+const express = require('express')
 
-const binaryTrees = require('./bench/binary-trees');
-const Logger = require('./bench/logger');
+const binaryTrees = require('./bench/binary-trees')
+const Logger = require('./bench/logger')
 
-const endpoints = require('./benchmarks.json');
-const app = express();
+const endpoints = require('./benchmarks.json')
+const app = express()
 
-const BENCH = "/bench/";
+const BENCH = "/bench/"
 
-const serveFile = (fileName, req, res) => fs.createReadStream(path.join(__dirname, './public/', fileName)).pipe(res);
-const serveIndex = serveFile.bind(null, 'index.html');
-const serveJS = serveFile.bind(null, 'main.js');
-const serveCSS = serveFile.bind(null, 'main.css');
+const serveFile = (fileName, req, res) => fs.createReadStream(path.join(__dirname, './public/', fileName)).pipe(res)
+const serveIndex = serveFile.bind(null, 'index.html')
+const serveJS = serveFile.bind(null, 'main.js')
+const serveCSS = serveFile.bind(null, 'main.css')
 
 const loggerHandler = (req, res, next) => {
   Logger({
@@ -34,58 +34,58 @@ const loggerHandler = (req, res, next) => {
 
 const binaryTreesHandler = (req, res, next) => {
 
-  const arg = req.params.arg;
-  const n = parseInt(arg);
+  const arg = req.params.arg
+  const n = parseInt(arg)
 
   if (isNaN(n) || (n.toString() !== arg)) {
     return next({
       status: 400,
       message: util.format('Bad Request: `%s` is not a number', arg)
-    });
+    })
   }
 
   if (n > 25) {
     return next({
       status: 400,
       message: util.format('Bad Request: `argument` must be lower or equal then 25 (got %d)', n)
-    });
+    })
   }
 
-  res.status(200).json(binaryTrees(n));
+  res.status(200).json(binaryTrees(n))
 };
 
 const routesHandler = (req, res) => {
 
-  let response = '<pre>';
+  let response = '<pre>'
 
   Object.keys(endpoints).forEach(key => {
-    const link = endpoints[key];
-    response += `<a href="${link}">${link}</a></br>`;
+    const link = endpoints[key]
+    response += `<a href="${link}">${link}</a></br>`
   });
 
-  response += '</pre>';
+  response += '</pre>'
 
-  res.status(200).send(response);
+  res.status(200).send(response)
 };
 
-const staticRouter = express.Router();
-const benchRouter = express.Router();
+const staticRouter = express.Router()
+const benchRouter = express.Router()
 
-staticRouter.get('/', routesHandler);
-staticRouter.get('*.js', serveJS);
-staticRouter.get('*.css', serveCSS);
+staticRouter.get('/', routesHandler)
+staticRouter.get('*.js', serveJS)
+staticRouter.get('*.css', serveCSS)
 
 Object.keys(endpoints).forEach((key) => {
-  const url = endpoints[key];
-  staticRouter.get(url, serveIndex);
-});
+  const url = endpoints[key]
+  staticRouter.get(url, serveIndex)
+})
 
-benchRouter.get(`${endpoints.BINARY_TREES}:arg`, binaryTreesHandler);
-benchRouter.get(`${endpoints.LOGGER}:arg`, loggerHandler);
+benchRouter.get(`${endpoints.BINARY_TREES}:arg`, binaryTreesHandler)
+benchRouter.get(`${endpoints.LOGGER}:arg`, loggerHandler)
 
 
-app.use('/', staticRouter);
-app.use(BENCH, benchRouter);
+app.use('/', staticRouter)
+app.use(BENCH, benchRouter)
 
 
 // catch 404 and forward to error handler
@@ -93,28 +93,28 @@ app.use((req, res, next) => {
   next({
     status: 404,
     message: 'Not Found'
-  });
-});
+  })
+})
 
 // error handler
 app.use((err, req, res, next) => {
   if (res.headersSent) {
-    return next(err);
+    return next(err)
   }
   res.status(err.status || 500)
     .send(err.message)
-});
+})
 
 if (cluster.isMaster) {
   for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
+    cluster.fork()
   }
   console.log("Server is listening at http://localhost:8002")
 
 } else {
 
   http.createServer(app)
-    .listen(8002);
+    .listen(8002)
 }
 
 
